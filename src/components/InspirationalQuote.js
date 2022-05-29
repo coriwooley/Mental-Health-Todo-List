599f5cbabee11921b436c5c4a224d24fa3850a00
Cori Wooley <wooleycori@gmail.com>
2022-05-29 05:08:49 -0800

    cheese

--- src/components/InspirationalQuote.js
+++ src/components/InspirationalQuote.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import boxicons from "boxicons"
 
 export default function InspirationalQuote(){
     const [quote, setQuote] = useState("You've got this")
@@ -8,6 +9,7 @@ export default function InspirationalQuote(){
 return (
     <div>
         <h1>{quote}</h1>
+        <box-icon name="cheese"></box-icon>
     </div>
 )
 }
